4cfecff3529d175d0e75e426188b024f61255583
kleonova <kleonova0@gmail.com>
2023-03-11 22:46:15 +0800

    add files

--- src/lesson1/CounterFn.js
+++ src/lesson1/CounterFn.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import PropTypes from 'prop-types';
 
 function CounterFn({ min, max }) {
     // magic сокрытие реализации
     // useState - is hooki - возвращает массив из объекта и функции изменения этого объекта
-    let [counterValue, setCounterValue] = useState(1);
-    
+    let [counterValue, setCounterValue] = useState(min);
+
     console.log("init CounterFn");
 
     // функция будет вызываться каждый раз при изменении компонента :(
@@ -16,10 +17,26 @@ function CounterFn({ min, max }) {
         }
     }
 
-    return <div>
-        <span>{ counterValue }</span>
-        <button type="button" onClick={ increnment }>+</button>
-    </div>;
+    function decrement() {
+        console.log("CounterFn: call decrement function");
+
+        if(counterValue > min) {
+            setCounterValue(counterValue-1)
+        }
+    }
+
+    return (
+        <div>
+            <button type="button" onClick={ decrement }>-</button>
+            <span>{ counterValue }</span>
+            <button type="button" onClick={ increnment }>+</button>
+        </div>
+    );
+}
+
+CounterFn.propTypes = {
+    min: PropTypes.number.isRequired,
+    max: PropTypes.number.isRequired,
 }
 
 export default CounterFn;
