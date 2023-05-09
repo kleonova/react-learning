c9f66acafb2be4164a8235b8d5df24a0c6993144
kleonova <kleonova0@gmail.com>
2023-05-09 09:09:39 +0800

    add bootstrap

--- src/lesson3/index.js
+++ src/lesson3/index.js
@@ -1,14 +1,37 @@
 import { useState } from "react";
 import ModalCard from "../components/ModalCard";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function() {    
     let [ isOpenModal, openModal ] = useState(false);
 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <>
+        <div className="p-2">
             <h1>Lesson #3</h1>
             <hr />
-            <button onClick={() => openModal(!isOpenModal)}>{isOpenModal ? 'Close modal' : 'Open modal' }</button>
+            <div className="d-flex">
+                <Button 
+                    variant="primary" 
+                    className="me-2"
+                    onClick={() => openModal(!isOpenModal)}
+                    >
+                        {isOpenModal ? 'Close my modal' : 'Open my modal' }
+                </Button>
+
+                <Button 
+                    variant="secondary" 
+                    className=""
+                    onClick={handleShow}
+                >
+                    Open bootstrap modal
+                </Button>
+            </div>
+
             { isOpenModal && 
                 <ModalCard 
                     header='My modal'
@@ -17,6 +40,21 @@ export default function() {
                     some text
                 </ModalCard> 
             }
-        </>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>    
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                    Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     );
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ContainerLesson3 from "./lesson3";
 
 const container = document.getElementById("root");
